f80842827accaf6dbe39391cdf36ff4cb3dfe966
Alyson Monteiro <alysonmonteironovo@gmail.com>
2025-09-29 10:35:01 -0300

    Aplicando a utilização de Contrutores no codigo de exemplo de produtos

---
+++ ExCotacaoDolarMembrosEstaticos/ExCotacaoDolarMembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExCotacaoDolarMembrosEstaticos
+{
+    internal class ConversorDeMoeda
+    {
+        public static double CotacaoDolar;
+        public static double dolares;
+        
+
+        public static double ConverterDolarParaReal(double dolares)
+        {
+            double taxaIOF = 0.06;
+            double valorEmReais = dolares * CotacaoDolar;
+
+            return  valorEmReais + (valorEmReais * taxaIOF);
+        }
+    }
+}
---
+++ ExCotacaoDolarMembrosEstaticos/ExCotacaoDolarMembrosEstaticos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using ExCotacaoDolarMembrosEstaticos;
+
+namespace ExCotacaoDolarMembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("COMPRA DE DOLAR:");
+            Console.Write("Qual a cotação do dólar? ");
+            ConversorDeMoeda.CotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dolares você vai comprar? ");
+            ConversorDeMoeda.dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Valor a ser pago em reais = " 
+                       + ConversorDeMoeda.ConverterDolarParaReal(ConversorDeMoeda.dolares).ToString("F2"));
+
+        }
+    }
+}
