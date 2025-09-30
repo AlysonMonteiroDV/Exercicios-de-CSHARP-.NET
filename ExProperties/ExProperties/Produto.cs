8b1b8552dc0e4e05012a2b05e168622754610750
Alyson Monteiro <alysonmonteironovo@gmail.com>
2025-09-30 11:41:24 -0300

    Utilizando a Properties para melhorar a estrutura do codigo e aplicar a logica

---
+++ ExProperties/ExProperties/Produto.cs
@@ -0,0 +1,69 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+namespace Course
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+
+        //Properties 
+        //Value: palavra reservada que representa o valor que está sendo atribuído à propriedade
+        //A palavra Value é 
+        public String Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double Preco
+        {
+            get { return _preco; }
+        }
+        public int Quantidade
+        {
+            get { return _quantidade; }
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+
+
+        }
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
