681ea7190d2c771bc5d8a6549b25f1470148425b
Alyson Monteiro <alysonmonteironovo@gmail.com>
2025-10-01 12:37:58 -0300

    Exercicio de Fixação de conhecimento nos contexto de emcapsumaneto, properties contrutores sobrecarga

---
+++ ExFixacaoContaBancaria/ExFixacaoContaBancaria/ContaBancaria.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExFixacaoContaBancaria
+{
+    internal class ContaBancaria
+    {
+
+        //Atributos publicos com get e set
+        public int NumConta { get; private set; }
+        public string NomeTitular { get; set; }
+        public double SaldoConta { get; private set; }
+
+        //Construtores
+
+        public ContaBancaria()
+        {
+        }
+        public ContaBancaria(int numConta, string nomeTitular, double saldoConta)
+        {
+            NumConta = numConta;
+            NomeTitular = nomeTitular;
+            SaldoConta = saldoConta;
+        }
+
+
+        public ContaBancaria(int numConta, string nomeTitular)
+        {
+            NumConta = numConta;
+            NomeTitular = nomeTitular;
+            SaldoConta = 0.0;
+        }
+
+        //Metodos
+        public double DepositoInicial(double deposito)
+        {
+            return SaldoConta += deposito;
+        }
+
+        public double Deposito(double deposito)
+        {
+            return SaldoConta += deposito;
+        }
+
+        public double Saque(double saque)
+        {
+            return SaldoConta -= saque + 5;
+        }
+
+        public void AdicionarNumConta(int conta)
+        {
+            NumConta = conta;
+        }
+
+
+        public override string ToString()
+        {
+            return "Conta: "
+                + NumConta
+                + ", Titular: "
+                + NomeTitular
+                + ", Saldo: $ "
+                + SaldoConta.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ ExFixacaoContaBancaria/ExFixacaoContaBancaria/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+
+
+
+namespace ExFixacaoContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("CONTA BANCARIA");
+
+            Console.Write("Entre com o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string Titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string deposito = Console.ReadLine().ToLower();
+
+            double ValorConta; //variavel para armazenar o valor da conta
+            if (deposito == "s")
+            {
+                Console.Write("Entre com o valor do deposito: ");
+                ValorConta = double.Parse(Console.ReadLine());
+            }
+            else
+            {
+                ValorConta = 0.0;
+            }
+
+            ContaBancaria conta = new ContaBancaria(numeroConta, Titular, ValorConta);
+
+            MostrarConta(conta);
+         
+            Console.WriteLine();
+            Console.Write("Entre com o valor para outro depósito: ");
+            ValorConta = conta.Deposito(double.Parse(Console.ReadLine()));
+
+            MostrarConta(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            ValorConta = conta.Saque(double.Parse(Console.ReadLine()));
+
+            MostrarConta(conta);
+
+        }
+
+        static void MostrarConta(object conta )
+        {
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+        }
+    }
+}
